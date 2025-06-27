0bab2bd2e6b3a7604284771a70406cd963dbb77e
IT Trainee <IT.Trainee@PSC_DC1.LOCAL>
2025-06-27 18:35:39 +0300

    updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'users',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'extensions'
 ]
 AUTH_USER_MODEL = 'users.User'
 MIDDLEWARE = [
--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/', include('myapp.urls')),
-    path('api/items/', include('lostfound.urls'))
+    path('api/items/', include('lostfound.urls')),
+    path('api/', include('extensions.urls')),
 ]
